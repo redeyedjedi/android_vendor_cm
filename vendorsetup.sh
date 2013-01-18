0cf0f275675f853aefb5c80b1d2138f666c1ef22
redeyedjedi <utahsbraincell@gmail.com>
2013-01-18 04:24:26 -0600

    initial changes to boot cm10.1

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,2 @@
-for combo in $(wget -o /dev/null -O - https://raw.github.com/CyanogenMod/hudson/master/cm-build-targets | grep cm-10.1 | awk {'print $1'})
-do
-    add_lunch_combo $combo
-done
+add_lunch_combo smba1002-userdebug
+add_lunch_combo cm_smba1002-userdebug
