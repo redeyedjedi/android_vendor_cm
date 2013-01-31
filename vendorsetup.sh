04b0f0a1f1cc992472c29c2c69bc49850c98d1fa
redeyedjedi <utahsbraincell@gmail.com>
2013-01-31 14:36:22 -0600

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,5 @@
-add_lunch_combo smba1002-userdebug
-add_lunch_combo cm_smba1002-userdebug
+for combo in $(wget -o /dev/null -O - https://raw.github.com/CyanogenMod/hudson/master/cm-build-targets | grep cm-10.1 | awk {'print $1'})
+
+do
+    add_lunch_combo $combo
+done
